10af32ae2e6d1d6d01680c1db77f6e3d50aa75ad
bendem <online@bendem.be>
2014-07-23 23:42:20 +0200

    Add a "post to report server" method thingy

--- src/main/java/io/statik/v1/StatikReportingTask.java
+++ src/main/java/io/statik/v1/StatikReportingTask.java
@@ -3,6 +3,12 @@ package io.statik.v1;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Map;
 
 /**
@@ -26,5 +32,35 @@ final class StatikReportingTask extends BukkitRunnable {
         // TODO Json da stuff
         // TODO Send da stuff
         // TODO Some DEBUG log?
+    }
+
+    public String post(final String json) throws IOException {
+        // TODO Do we compress data?
+        final URL url = new URL(null); // TODO Should be statik.STATIK_ENDPOINT
+        final URLConnection connection = url.openConnection();
+        final byte[] data = json.getBytes();
+
+        // Headers
+        connection.addRequestProperty("User-Agent", ""); // TODO What do we put here?
+        connection.addRequestProperty("Content-Type", "application/json");
+        connection.addRequestProperty("Content-Length", String.valueOf(data.length));
+        connection.addRequestProperty("Accept", "application/json");
+
+        connection.setDoOutput(true);
+
+        // Write the data
+        final OutputStream os = connection.getOutputStream();
+        os.write(data);
+        os.flush();
+
+        // Read the response
+        final BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        final String response = br.readLine();
+
+        // close resources
+        os.close();
+        br.close();
+
+        return response;
     }
 }
